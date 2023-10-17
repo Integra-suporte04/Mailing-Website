6e0eceed0f744b117aa54d6a745fcba98e868aeb
Integra-suporte04 <suporte04@integrasistema.com>
2023-10-17 17:11:18 -0300

    adjust tabela_mailing

--- IntegraMailing/IntegraMailing/IntegraMailing/Data/tabela_mailing.cs
+++ IntegraMailing/IntegraMailing/IntegraMailing/Data/tabela_mailing.cs
@@ -5,7 +5,7 @@ namespace IntegraMailing.Data
     public class tabela_mailing
     {
         [Key]
-        public int Id { get; }
+        public int Id { get; set; }
         public string? numero;
         public int campanha_Id { get; set; }
 
